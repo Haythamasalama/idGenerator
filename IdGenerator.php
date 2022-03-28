c1f9c41dc8aadc3abd4ec01c6e40d7c50982a42d
Haytham Salama <haythamasalama@gmail.com>
2022-03-28 19:26:52 +0200

    add divider and ids attributes

--- IdGenerator.php
+++ IdGenerator.php
@@ -3,4 +3,20 @@
 
 class IdGenerator
 {
+
+    /**
+     *  divider between sections
+     * - , / , *
+     * @var string
+     */
+    protected string $divider = '-';
+
+    /**
+     *  array of section that generated
+     *  Ha-2001-07-16-S
+     * @var array
+     */
+    protected array $id = [];
+
+
 }
