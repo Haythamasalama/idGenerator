d95c4558638442bf71840bd8f720c75eb8e703ec
Haytham Salama <haythamasalama@gmail.com>
2022-03-28 19:27:49 +0200

    add date function

--- IdGenerator.php
+++ IdGenerator.php
@@ -19,4 +19,17 @@ class IdGenerator
     protected array $id = [];
 
 
+    /**
+     *  data section
+     *  for example  :Y-m-d , Ymd ,Y ,m ...etc
+     * @param string $format Y-m-d , Ymd ,Y
+     * @return $this
+     */
+    public  function  date(string $format='Ym') : static
+    {
+        $this->id []= date($format);
+
+        return $this;
+    }
+
 }
